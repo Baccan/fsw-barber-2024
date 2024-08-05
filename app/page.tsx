cef36154f1e4baeb69e07698bc885d1ce2503355
Gustavo Baccan Gomes <gustavobaccang@gmail.com>
2024-08-05 12:59:07 -0300

    [CORE] shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,15 @@
 // To create a client component, you can use the client component API.
 "use client"
 
-import { useState } from "react"
+// import { useState } from "react"
+import { Button } from "./_components/ui/button"
 
 const Home = () => {
-  const [] = useState()
+  // const [] = useState()
 
-  return <h1 className="bg-red mb-5 text-red-500">Red text</h1>
+  // return <h1 className="bg-red mb-5 text-red-500">Red text</h1>
+
+  return <Button>Click me</Button>
 }
 
 export default Home
