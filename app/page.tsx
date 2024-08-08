9bcc9d100190c300b252c53b52b263cc5a596c38
Gustavo Baccan Gomes <gustavobaccang@gmail.com>
2024-08-07 22:26:41 -0300

    [FEATURE] add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,10 @@ import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
 import { SearchIcon } from "lucide-react"
 import { Button } from "./_components/ui/button"
+import { Card, CardContent } from "./_components/ui/card"
 import Image from "next/image"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   // const [] = useState()
@@ -20,11 +23,15 @@ const Home = () => {
   // return <Button>Click me</Button>
   return (
     <div>
+      {/* HEADER */}
       <Header />
+
+      {/* TEXTO */}
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá Gustavo</h2>
         <p>Segunda-feira, 05 de Agosto.</p>
 
+        {/* BUSCA */}
         <div className="flex items-center gap-2 space-x-2">
           <Input placeholder="Faça sua busca..." />
           <Button>
@@ -32,6 +39,7 @@ const Home = () => {
           </Button>
         </div>
 
+        {/* BANNER */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             src="/banner-01.svg"
@@ -40,6 +48,31 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* AGENDAMENTO */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="gap2 flex items-center">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
