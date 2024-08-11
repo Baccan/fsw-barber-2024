4662f456af1a288828ae5e7ad07dcfce3fca9b9d
Gustavo Baccan Gomes <gustavobaccang@gmail.com>
2024-08-10 21:13:49 -0300

    [FEATURE] add PhoneItem component and Footer to Layout

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
 import { SearchIcon } from "lucide-react"
 import { Button } from "./_components/ui/button"
-import { Card, CardContent } from "./_components/ui/card"
 import Image from "next/image"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
@@ -97,16 +96,6 @@ const Home = async () => {
           ))}
         </div>
       </div>
-
-      <footer>
-        <Card>
-          <CardContent className="px-5 py-6">
-            <p className="text-sm text-gray-400">
-              @ 2024 Copyrigth <span className="font-bold">FSW Barber</span>
-            </p>
-          </CardContent>
-        </Card>
-      </footer>
     </div>
   )
 }
